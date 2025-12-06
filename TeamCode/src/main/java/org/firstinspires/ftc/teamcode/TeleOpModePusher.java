880e68cd8b7ceefe9a0a6d664666e35438fb3d6c
JacobEngelbert <nkleinjan@gmail.com>
2025-12-06 10:10:05 -0600

    added pusher from last year

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModePusher.java
@@ -0,0 +1,179 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name = "$$$ TELE-A (Pusher)", group = "$$$")
+@Disabled
+public class TeleOpModePusher extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // main wheel drive motors
+    private DcMotorEx leftFrontDrive = null;
+    private DcMotorEx rightFrontDrive = null;
+    private DcMotorEx leftBackDrive = null;
+    private DcMotorEx rightBackDrive = null;
+
+    @Override
+    public void runOpMode() {
+
+        // main wheel drive motor hardware names
+        leftFrontDrive = hardwareMap.get(DcMotorEx.class, "front_left_drive");
+        rightFrontDrive = hardwareMap.get(DcMotorEx.class, "front_right_drive");
+        leftBackDrive = hardwareMap.get(DcMotorEx.class, "back_left_drive");
+        rightBackDrive = hardwareMap.get(DcMotorEx.class, "back_right_drive");
+
+        // assign wheel motor directions
+        rightFrontDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotorEx.Direction.REVERSE);
+
+        // outside the while loop, set homing mode false
+        double driveDirectionFactor = 1.0;
+
+        // *******************************************************************************************
+        // Wait for the game to start (driver presses START)
+        // *******************************************************************************************
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // *******************************************************************************************
+            // SECTION 1: Read Inputs
+            // *******************************************************************************************
+
+            // *******************************************************************************************
+            // SECTION 2: Robot Wheels / Joysticks / Dpad
+            // *******************************************************************************************
+
+            // toggle backwards mode to use the pusher
+            // --------------------------------------------------------------------------------------------
+            if (gamepad1.back || gamepad2.back) {
+                driveDirectionFactor = -1.0;
+            } else if (gamepad1.start || gamepad2.start) {
+                driveDirectionFactor = 1.0;
+            }
+
+            // turbo mode speed overrides with left and right stick buttons
+            // --------------------------------------------------------------------------------------------
+            double turboOverrideLeftStick = 0.25;
+            double turboOverrideRightStick = 0.25;
+            // max wheel speed override
+            if (gamepad1.left_stick_button || gamepad2.left_stick_button) {
+                turboOverrideLeftStick = 0.60;
+            }
+            if (gamepad1.right_stick_button || gamepad2.right_stick_button) {
+                turboOverrideRightStick = 0.60;
+            }
+
+            // start of code from ftc robot controller external examples
+            // --------------------------------------------------------------------------------------------
+
+            double max;
+
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            double axial = 0;
+            double lateral = 0;
+            double yaw = 0;
+
+            // gamepad 1
+            if (Math.abs(gamepad1.left_stick_y) > 0.1) {
+                // Note: pushing stick forward gives negative value
+                axial = -gamepad1.left_stick_y * turboOverrideLeftStick * driveDirectionFactor;
+            }
+            if (Math.abs(gamepad1.left_stick_x) > 0.1) {
+                lateral = gamepad1.left_stick_x * turboOverrideLeftStick * driveDirectionFactor;
+            }
+            if (Math.abs(gamepad1.right_stick_x) > 0.1) {
+                yaw = gamepad1.right_stick_x * turboOverrideRightStick; // * driveDirectionFactor;
+            }
+
+            // gamepad 2
+            if (Math.abs(gamepad2.left_stick_y) > 0.1) {
+                // Note: pushing stick forward gives negative value
+                axial = -gamepad2.left_stick_y * turboOverrideLeftStick * driveDirectionFactor;
+            }
+            if (Math.abs(gamepad2.left_stick_x) > 0.1) {
+                lateral = gamepad2.left_stick_x * turboOverrideLeftStick * driveDirectionFactor;
+            }
+            if (Math.abs(gamepad2.right_stick_x) > 0.1) {
+                yaw = gamepad2.right_stick_x * turboOverrideRightStick; // * driveDirectionFactor;
+            }
+
+            // D-Pad overrides for the same joystick functions as above
+            // --------------------------------------------------------------------------------------------
+            // TODO: adjust max speed between 0.0 and 1.0
+            double precisionMax = 0.20;
+            if (!(gamepad1.dpad_up && gamepad1.dpad_down && gamepad1.dpad_left && gamepad1.dpad_right)) {
+                if (gamepad1.dpad_up || gamepad2.dpad_up) {
+                    axial = precisionMax * driveDirectionFactor;
+                }
+                if (gamepad1.dpad_down || gamepad2.dpad_down) {
+                    axial = -precisionMax * driveDirectionFactor;
+                }
+                if (gamepad1.dpad_left || gamepad2.dpad_left) {
+                    lateral = -precisionMax * driveDirectionFactor;
+                }
+                if (gamepad1.dpad_right || gamepad2.dpad_right) {
+                    lateral = precisionMax * driveDirectionFactor;
+                }
+            }
+
+            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Set up a variable for each drive wheel to save the power level for telemetry.
+            double leftFrontPower = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower = axial - lateral + yaw;
+            double rightBackPower = axial + lateral - yaw;
+
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower /= max;
+                rightFrontPower /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
+            }
+
+            // *******************************************************************************************
+            // SECTION 6: Write Outputs
+            // *******************************************************************************************
+
+            // power to wheels
+            leftFrontDrive.setPower(leftFrontPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            rightBackDrive.setPower(rightBackPower);
+            leftBackDrive.setPower(leftBackPower);
+
+            // *******************************************************************************************
+            // SECTION 7: Telemetry
+            // *******************************************************************************************
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Run Time", runtime.toString());
+            telemetry.addData("Run Mode", driveDirectionFactor < 0 ? "!!! PUSHER FORWARD !!!" : "!!! CLAW FORWARD !!!");
+            telemetry.addLine(String.format("[%s]----[%s]", leftFrontPower, rightFrontPower));
+            telemetry.addLine(String.format("[%s]----[%s]", leftBackPower, rightBackPower));
+            telemetry.addLine(String.format("Turbo: L[%4.2f] R[%4.2f]", turboOverrideLeftStick, turboOverrideRightStick));
+
+            // Update telemetry
+            telemetry.update();
+            idle();
+
+        }
+    }
+
+}
