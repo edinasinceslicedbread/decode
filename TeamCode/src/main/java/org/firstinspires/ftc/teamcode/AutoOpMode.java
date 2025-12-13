b26599fb2a77b593e1f525a76515c54e5e3a8e0d
kseniiaprotsko <kseniia.protsko@gmail.com>
2025-12-13 10:50:55 -0600

    distance and speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
@@ -72,8 +72,8 @@ public class AutoOpMode extends OpMode {
      * velocity. Here we are setting the target, and minimum velocity that the launcher should run
      * at. The minimum velocity is a threshold for determining when to fire.
      */
-    final double LAUNCHER_TARGET_VELOCITY = 1375;
-    final double LAUNCHER_MIN_VELOCITY = 1300;
+    final double LAUNCHER_TARGET_VELOCITY = 1400;
+    final double LAUNCHER_MIN_VELOCITY = 1335;
 
     // Declare OpMode members.
 
@@ -223,7 +223,7 @@ public class AutoOpMode extends OpMode {
 
         switch (launchState) {
             case IDLE:
-                if (feederTimer.seconds() > 1.50) {
+                if (feederTimer.seconds() > 1.25) {
                     backLeftDrive.setPower(0.0);
                     backRightDrive.setPower(0.0);
                     frontLeftDrive.setPower(0.0);
